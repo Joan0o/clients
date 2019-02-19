adb46b5cf3405f6cfc070122438701db78fb2e75
Joan Varon <joan@timemanagerweb.com>
2019-02-19 15:52:31 -0500

    start pointi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'material-components-web';
+import * as mdc from 'material-components-web';
+
+const checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('.mdc-checkbox'));
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="react-logo" alt="logo" />
+
+
+          <button className="mdc-icon-button"
+                  aria-label="Add to favorites"
+                  aria-pressed="false"
+                  data-mdc-auto-init="MDCIconButtonToggle">
+            <i className="material-icons mdc-icon-button__icon mdc-icon-button__icon--on">favorite</i>
+            <i className="material-icons mdc-icon-button__icon">favorite_border</i>
+          </button>
+
         </header>
       </div>
     );
